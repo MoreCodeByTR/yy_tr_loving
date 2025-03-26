765111fd443c8b304684a46cc0bb7e1566ee5d10
tianrui0526 <tianrui0526@wepie.com>
2025-03-26 10:15:02 +0800

    feat: 路由basename

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import { defineAppConfig } from 'ice';
 
 // App config, see https://v3.ice.work/docs/guide/basic/app
-export default defineAppConfig(() => ({}));
+export default defineAppConfig(() => ({
+  // Set your configs here.
+  router: {
+    basename: '/yy_tr_loving/',
+  },
+}));
